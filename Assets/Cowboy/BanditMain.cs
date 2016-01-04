b299826b5d33a9d5614f14fada12986bc3847db6
pixelshade <psuldo@gmail.com>
2016-01-04 12:55:30 +0100

    init
    gg

---
+++ Assets/Cowboy/BanditMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class BanditMain : MonoBehaviour {
+    private NavMeshAgent agent;
+	// Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+        var gos = GameObject.FindGameObjectsWithTag("TREASURE");
+        // we choose random target
+        var goalId = Random.Range(0, gos.Length - 1);
+        var goal = gos[goalId];
+        agent.destination = goal.GetComponent<Transform>().position;
+
+    }
+
+    
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnCollisionEntrer(Collision col)
+    {
+        if (col.gameObject.name == "treasure")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
