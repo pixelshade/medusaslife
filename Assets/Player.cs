9202a75aba76171fb8a27d518c570961fbc27b2c
pixelshade <psuldo@gmail.com>
2016-01-09 21:31:47 +0100

    sight based turning to stone, particle emision

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,62 +7,83 @@ public class Player : MonoBehaviour {
     private Vector2 uvOffset = Vector2.zero;
     private Vector2 uvAnimationRate = new Vector2(15, 0);
     public GameObject laserHitParticleSystem;
+    public GameObject VisionCollider;
+    private ParticleSystem medusaVisionParticles;
 
     private GameObject hitParticleSystem;
     // Use this for initialization
     void Start () {
-        cam = Camera.main.transform;
+        cam = this.gameObject.GetComponentInChildren<Camera>().transform;
+        //cam = Camera.main.transform;
         laserLine = GetComponent<LineRenderer>();
         laserLine.SetWidth(0.2f, 0.2f);
         hitParticleSystem = Instantiate(laserHitParticleSystem);
         hitParticleSystem.SetActive(false);
+        medusaVisionParticles = gameObject.GetComponentInChildren<ParticleSystem>();
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButton(0))
         {
-            RaycastHit hit;
-            // get the forward vector of the player's camera 
-            //Vector3 fwd = cam.TransformDirection(cam.forward);
-            //Laser(transform.position, transform.position + fwd);
-
-            Ray ray = new Ray(transform.position, cam.forward);            
-            if (Physics.Raycast(ray, out hit))
-            {
-                Laser(ray.origin, hit.point);
-                hitParticleSystem.transform.position = hit.point;                
-                hitParticleSystem.SetActive(true);
-                //if there something in our front, check if it's the monolith
-                if (hit.collider.gameObject.CompareTag("Enemy"))
-                {
-                    
-                    var bandit = hit.collider.gameObject.GetComponent<BanditMain>();
-                    if (bandit == null) return;
-                    bandit.StartTurningToStone();
-
-                }
-                
-            } else
-            {
-                Laser(ray.origin, ray.GetPoint(50));
-                hitParticleSystem.SetActive(false);
-            }
-
+            //Fire();
+            //if (medusaVisionParticles.isPaused)            
+                //medusaVisionParticles.Play();
+            medusaVisionParticles.enableEmission = true;
+            TurnToStone(true);
         }else
         {
-            hitParticleSystem.SetActive(false);
-            Laser(Vector3.zero, Vector3.zero);
+            TurnToStone(false);
+            medusaVisionParticles.enableEmission = false;
+            //if (!medusaVisionParticles.isPaused)
+            //{
+            //    medusaVisionParticles.Pause();
+            //    medusaVisionParticles.Clear();
+            //}
+                
+            // turn of laser
+            //hitParticleSystem.SetActive(false);
+            //Laser(Vector3.zero, Vector3.zero);
         }
-        
+    }
 
+    void TurnToStone(bool turnToStone)
+    {
+        if (VisionCollider == null) { Debug.LogError("Vision collider not set"); return; }
+        var vc = VisionCollider.GetComponent<VisionCollider>();
+        vc.TurnToStoneOnCollision = turnToStone;
+    }
 
-        //var ray = new Ray(cam.position, cam.forward);
-        //RaycastHit hit;
-        //if (Physics.Raycast(ray,
-        //{
+    void Fire()
+    {
+        RaycastHit hit;
+        // get the forward vector of the player's camera 
+        Vector3 fwd = cam.TransformDirection(cam.forward);
+        //Laser(transform.position, transform.position + fwd);
 
-        //}
+        Ray ray = new Ray(transform.position, fwd);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Laser(ray.origin, hit.point);
+            hitParticleSystem.transform.position = hit.point;
+            hitParticleSystem.SetActive(true);
+            //if there something in our front, check if it's the monolith
+            if (hit.collider.gameObject.CompareTag("Enemy"))
+            {
+
+                var bandit = hit.collider.gameObject.GetComponent<BanditMain>();
+                if (bandit == null) return;
+                bandit.StartTurningToStone();
+
+            }
+
+        }
+        else
+        {
+            Laser(ray.origin, ray.GetPoint(50));
+            hitParticleSystem.SetActive(false);
+        }
     }
 
 
---
+++ Assets/Scripts/VisionCollider.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionCollider : MonoBehaviour {
+    public bool TurnToStoneOnCollision = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider col)
+    {        
+        if (!TurnToStoneOnCollision) return;        
+
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            var bandit = col.gameObject.GetComponent<BanditMain>();
+            if (bandit == null) return;
+            bandit.StartTurningToStone();
+        }
+    }
+
+    //void OnTriggerExit(Collider col)
+    //{
+    //    if (col.gameObject.CompareTag("Enemy"))
+    //    {
+    //        var bandit = col.gameObject.GetComponent<BanditMain>();
+    //        if (bandit == null) return;
+    //        bandit.StartTurningToStone();
+
+    //    }
+    //}
+}
